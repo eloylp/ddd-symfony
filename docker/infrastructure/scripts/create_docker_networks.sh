41c1548ec8e1171a6ddd6f23c60a93a41032e9fb
eloylp <eloy@sandboxwebs.com>
2017-03-26 18:23:37 +0200

    Added centralized loging infrastructure with rsyslog.

--- docker/infrastructure/scripts/create_docker_networks.sh
+++ docker/infrastructure/scripts/create_docker_networks.sh
@@ -5,3 +5,4 @@ docker network create infrastructure_sql_traffic
 docker network create infrastructure_nosql_traffic
 docker network create infrastructure_amqp_traffic
 docker network create infrastructure_redis_traffic
+docker network create infrastructure_rsyslog_traffic
