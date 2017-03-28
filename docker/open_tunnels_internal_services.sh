5ad6afc09c30891ef46f8376d4effcb83eb7b03a
eloylp <eloy@sandboxwebs.com>
2017-03-28 18:14:06 +0200

    Added mysql to infrastructure tunnels script.

--- docker/open_tunnels_internal_services.sh
+++ docker/open_tunnels_internal_services.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ssh -f -N -L 27017:mongodb:27017 root@localhost -p2222
+ssh -f -N -L 3306:mariadb:3306 root@localhost -p2222
 ssh -f -N -L 15672:rabbitmq:15672 root@localhost -p2222
